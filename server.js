0a25ed83518d98bbac23272ed6743421bf2f6ed7
Valera Sokolov <komproekt42@gmail.com>
2022-08-03 12:54:57 +0300

    Ver

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({json: true}));
+
+let posts = [];
+let nextId = 1;
+
+const router = new Router();
+
+router.get('/posts', async (ctx, next) => {
+    ctx.response.body = posts;
+});
+
+
+router.post('/posts', async(ctx, next) => {
+    const data = JSON.parse(ctx.request.body);
+   
+    if (data.id !== undefined) {
+        posts = posts.map(o => o.id !== data.id ? o : {...o, content: data.content});
+        ctx.response.status = 204;
+        return;
+    }
+
+    posts.push({...data, id: nextId++, created: new Date()});
+    ctx.response.status = 204;
+});
+
+router.delete('/posts/:id', async(ctx, next) => {
+    const postId = Number(ctx.params.id);
+    console.log(postId)
+    const index = posts.findIndex(o => o.id === postId);
+    if (index !== -1) {
+        posts.splice(index, 1);
+    }
+    ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
